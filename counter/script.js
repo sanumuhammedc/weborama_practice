5cdc5e64b34ca8acccbd8a649845b34371da21d1
SANU MUHAMMED C <sanumuhammed.c@gmail.com>
2021-08-18 20:25:54 +0530

    added counter project

---
+++ counter/script.js
@@ -0,0 +1,18 @@
+let count=0;
+function increment(){
+  let val=document.getElementById("num").innerHTML;
+  let num=Number(val)+1
+  document.getElementById("num").innerHTML=num;
+}
+
+function decrement(){
+    let val=document.getElementById("num").innerHTML;
+    let num=Number(val)-1
+    document.getElementById("num").innerHTML=num;
+  }
+
+function reset(){
+    let val=document.getElementById("num").innerHTML;
+    let num=Number(val)*0
+    document.getElementById("num").innerHTML=num;
+}
